527c9f70b05b4a99bc68c122c0ad7c093fea86b0
cojimon <bajilisk4737@gmail.com>
2021-03-14 22:47:24 +0900

    3.14 更新

--- script/my_jsfile.js
+++ script/my_jsfile.js
@@ -1,7 +1,49 @@
 $(function() {
 
-  $('#gd-und').click(function(){
-    $('html').animate({scrollTop:0});
-  })
+  /*ガイド　トップ----------------*/
+  $(function(){
 
-});
+    var gd_top = $('#gd-top');
+    var em = 40;
+    
+    gd_top.css('display','none');
+
+    /*アニメーション*/
+    $(window).scroll(function(){
+      var scroll = $(this).scrollTop();
+      if(scroll < em){
+        gd_top.fadeOut();
+      } else {
+        gd_top.fadeIn();
+      }
+    });
+
+    /*クリック*/    
+    var re_position = $('#re-position').offset().top;
+
+    gdTop.click(function(){
+      $('html').animate({scrollTop:re_position});
+    });
+
+  });// /ガイドトップ
+
+   /*ガイド　アンダー-----------------*/
+    var gd_und = $('#gd-und');   
+
+    gd_und.click(function(){
+      $('html').animate({scrollTop:0});
+    });
+
+  /*ガイド　ミドル-----------------*/
+  var target = $('#gd-mid');
+  var el = target.offset().top;
+  $(window).scroll(function(){
+    var scrollM = $(this).scrollTop();
+    if (scrollM > el){
+      target.slideDown();
+    }
+
+  });
+
+
+});//---/function()---
