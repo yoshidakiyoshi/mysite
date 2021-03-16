1b9353492bbda2e376dac8c129533a523816c9f7
cojimon <bajilisk4737@gmail.com>
2021-03-16 22:03:22 +0900

    ガイド　イベント　完成

--- script/my_jsfile.js
+++ script/my_jsfile.js
@@ -8,7 +8,7 @@ $(function(){
 
       gd_top.css('display','none');
 
-      /*アニメーション*/
+      /*スクロール*/
       $(window).scroll(function(){
       var scroll = $(this).scrollTop();
       if(scroll < em){
@@ -19,53 +19,59 @@ $(function(){
       });
 
       /*クリック*/    
-      var re_position = $('#re-position').offset().top - 93;
-
       gd_top.click(function(){
-      $('html').animate({scrollTop:re_position});
+      $('html').animate({scrollTop:6292});
       });
 
 
   });/*ガイド　トップ*/
+
+
+
+  /*ガイド　ミドル-------------------------------*/
+  $(function() {
+
+      var gd_mid = $('#gd-mid');
+      /*スクロール*/
+      $(window).scroll(function(){
+      var scrollM = $(this).scrollTop();
+      if (scrollM < 1450 || scrollM > 6750){
+        gd_mid.fadeOut();
+      } else {
+        gd_mid.fadeIn();
+      }
+      });
+
+      /*クリック*/ 
+      gd_mid.click(function(){
+        $('html').animate({scrollTop:7480});
+      });
+
+  });/*ガイド　ミドル*/
+
 
   /*ガイド　アンダー-------------------------------*/
   $(function() {
 
       var gd_und = $('#gd-und');   
 
+      /*クリック*/ 
       gd_und.click(function(){
       $('html').animate({scrollTop:0});
       });
 
+      /*ホバー*/
+      
       gd_und.hover(function() {
- 
-      //マウスを乗せたら色が変わる
-        $(this).css({'transform': 'rotateX(180deg)',
+
+        $('#gd-und img').css({'transform': 'rotateX(180deg)',
       'transition':'all 200ms 0s ease'});
-     
-      //ここにはマウスを離したときの動作を記述
-      }, function() {
-     
-      //色指定を空欄にすれば元の色に戻る
-        $(this).css('transform', 'rotateX(0deg)');
-     
+
+      }, function() {    
+
+        $('#gd-und img').css('transform', 'rotateX(0deg)');      
       });
 
-  });/*ガイド　アンダー*/
-
-  /*ガイド　ミドル-------------------------------*/
-  $(function() {
-
-      var target = $('#gd-mid');
-      var el = target.offset().top;
-      $(window).scroll(function(){
-      var scrollM = $(this).scrollTop();
-      if (scrollM > el){
-      target.slideDown();
-      }
-
-      });/*ガイド　ミドル*/
-
-  });
+});/*ガイド　アンダー*/
 
 });
